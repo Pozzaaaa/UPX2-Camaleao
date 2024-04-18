23d73162ec2b91dd0c5bdc3000a7df9b99297043
Julia Pozza <jujubs.pozza@gamil.com>
2024-04-17 21:48:07 -0300

    Projeto

---
+++ Projeto/UPXHortaFacil/src/upxhortafacil/Menu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upxhortafacil;
+
+/**
+ *
+ * @author jujub
+ */
+public class Menu {
+    public Menu()
+    { System.out.println();
+            System.out.println("Bem-Vindo(a) ao Horta Facil");
+            System.out.println("---------------------");
+            System.out.println("Este e um sistema para cadatro de hortas e voluntarios");
+            System.out.println("voce pode se tornar um  volintario ou cadastrar sua propria horta!");
+    }
+}
---
+++ Projeto/UPXHortaFacil/src/upxhortafacil/UPXHortaFacil.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package upxhortafacil;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jujub
+ */
+public class UPXHortaFacil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        new Menu();
+    }
+    
+}
