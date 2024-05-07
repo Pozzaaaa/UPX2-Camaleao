3058b083c12a6d5028bb4248227990a6c9aed964
Julia Pozza <jujubs.pozza@gamil.com>
2024-05-06 22:41:42 -0300

    Codigo completo

---
+++ Projeto/UPX2HortaFacil/src/upx2hortafacil/Usuario.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upx2hortafacil;
+
+/**
+ *
+ * @author jujub
+ */
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+    private String cidade;
+    private String estado;
+
+    public Usuario(String nome, String cpf, String telefone, String endereco, String cidade, String estado) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.estado = estado;
+        this.cidade = cidade;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
